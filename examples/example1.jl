dfd6f04ae30453d61bcc8db0b9193f570a20f87a
Max Opgenoord <mopg@mit.edu>
2018-04-19 13:16:24 +0800

    Added node

--- examples/example1.jl
+++ examples/example1.jl
@@ -1,7 +1,13 @@
 using Tikz
 
-obj = tikzObj( "example.tex", standalone = true )
+colors = Dict( "blueblue" => [50.,60.,180.] )
 
-plot( obj, [0., 5., 2.], [1.,6.,-1], smooth=true )
+style = tikzStyle( "style1", lw = 2.0 )
+
+obj = tikzObj( "example.tex", standalone = true, colors = colors, styles = [style] )
+
+plot( obj, [0., 5., 2.], [1.,6.,-1], smooth=false, linestyle="--", color="blueblue", style=style )
+
+node( obj, 1.0, 3.0, color = "blueblue", extracommands="circle,fill=gray,inner sep = 0pt,minimum size = 5pt" )
 
 closeObj( obj )
---
+++ src/node.jl
@@ -0,0 +1,25 @@
+function node( obj::tikzObj, x::Float64, y::Float64; lw = 0.0, color = "",
+               linestyle="", style=tikzStyle(), extracommands="", name="", text="" )
+
+
+    n = length(x)
+
+    # print start
+    @printf( obj.fID, "\\node[" )
+    if length(color) > 0
+        @printf( obj.fID, "draw=%s, ", color )
+    end
+    if lw > 0.0
+        @printf( obj.fID, "line width=%3.2fpt, ", lw )
+    end
+    if style.set
+        @printf( obj.fID, "%s, ", style.name )
+    end
+    @printf( obj.fID, "%s] ", extracommands )
+
+    if length(name) > 0
+        @printf( obj.fID, " (%s) ", name )
+    end
+    @printf( obj.fID, " at ( %6.4f, %6.4f ) {%s};", x, y, text )
+
+end
